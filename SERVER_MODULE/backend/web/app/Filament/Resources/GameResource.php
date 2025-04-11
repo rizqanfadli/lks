2c71a42c2d9cf4bb3c1d09fd77748bb3914386f1
rizqanfadli <rizqanfadli883@gamil.com>
2025-04-11 09:15:46 +0700

    up

---
+++ SERVER_MODULE/backend/web/app/Filament/Resources/GameResource.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GameResource\Pages;
+use App\Filament\Resources\GameResource\RelationManagers;
+use App\Models\Game;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GameResource extends Resource
+{
+    protected static ?string $model = Game::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageGames::route('/'),
+        ];
+    }
+}
